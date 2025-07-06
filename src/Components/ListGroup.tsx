dbf533ef21bfa630d28607f6ba51ea6eda0b5713
Jack --Linux <donoher.jack@gmail.com>
2025-07-06 21:37:11 +0100

    state management

--- src/Components/ListGroup.tsx
+++ src/Components/ListGroup.tsx
@@ -1,18 +1,26 @@
-import type { MouseEvent } from "react";
+import { useState } from "react";
 
 function ListGroup() {
   let dogs = ["Amy", "Abbie", "Chloe", "Mia"];
+  const [selectedIndex, selectedStateIndex] = useState(-1);
 
-  const handleClick = (event: MouseEvent) => {
-    console.log(event);
-  };
   return (
     <>
       <h1>List</h1>
       <ul className="list-group">
         {dogs.length === 0 && <p>No dogs found!</p>}
-        {dogs.map((dog) => (
-          <li key={dog} onClick={handleClick}>
+        {dogs.map((dog, index) => (
+          <li
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            key={dog}
+            onClick={() => {
+              selectedStateIndex(index);
+            }}
+          >
             {dog}
           </li>
         ))}
