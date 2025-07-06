346ffecea2750ccab940a8e5e78423503fa3ab28
Jack --Linux <donoher.jack@gmail.com>
2025-07-06 21:22:35 +0100

    Events

--- src/Components/ListGroup.tsx
+++ src/Components/ListGroup.tsx
@@ -1,14 +1,20 @@
+import type { MouseEvent } from "react";
+
 function ListGroup() {
   let dogs = ["Amy", "Abbie", "Chloe", "Mia"];
-  dogs = [];
 
+  const handleClick = (event: MouseEvent) => {
+    console.log(event);
+  };
   return (
     <>
       <h1>List</h1>
       <ul className="list-group">
         {dogs.length === 0 && <p>No dogs found!</p>}
         {dogs.map((dog) => (
-          <li key={dog}>{dog}</li>
+          <li key={dog} onClick={handleClick}>
+            {dog}
+          </li>
         ))}
       </ul>
     </>
