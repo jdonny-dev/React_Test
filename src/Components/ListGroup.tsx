3d97be5db269cf1c5517393aacc5b547d45b7ef3
Jack --Linux <donoher.jack@gmail.com>
2025-07-06 21:06:31 +0100

    Conditional Rendering

--- src/Components/ListGroup.tsx
+++ src/Components/ListGroup.tsx
@@ -1,9 +1,12 @@
 function ListGroup() {
-  const dogs = ["Amy", "Abbie", "Chloe", "Mia"];
+  let dogs = ["Amy", "Abbie", "Chloe", "Mia"];
+  dogs = [];
+
   return (
     <>
       <h1>List</h1>
       <ul className="list-group">
+        {dogs.length === 0 && <p>No dogs found!</p>}
         {dogs.map((dog) => (
           <li key={dog}>{dog}</li>
         ))}
